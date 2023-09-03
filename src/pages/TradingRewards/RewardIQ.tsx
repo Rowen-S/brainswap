751ee1bbb75b070e6c86b78ddcf03c6372b992a9
Puloe.eth <shenzhihui@MacBook-Pro.local>
2023-09-03 14:17:09 +0800

    up power

--- src/pages/TradingRewards/RewardIQ.tsx
+++ src/pages/TradingRewards/RewardIQ.tsx
@@ -31,37 +31,48 @@ export default function RewardIQ() {
   const { loading, error, data } = useQuery<PowerProps>(GET_LP_TRAD_POWER, {
     client: tradingClient,
     variables: { user: account },
+    pollInterval: 5000,
+    notifyOnNetworkStatusChange: true,
   })
 
-  const myTotalPower = useMemo(() => {
-    if (!loading && !error && data && data.lpinfos.length && data.userMiningInfos.length) {
+  const myLpPower = useMemo(() => {
+    if (!loading && !error && data && data.lpinfos.length) {
       const liquidityStartDateTime = DateTime.fromSeconds(Number(data.lpinfos[0].liquidityStart))
       const timeDifferenceInSeconds = DateTime.now().diff(liquidityStartDateTime).as('seconds')
-      const countPower =
+      const countLpPower =
         (timeDifferenceInSeconds * Math.pow(parseFloat(data.lpinfos[0].amountTotalUSD), 0.7) * 10) / towWeek
-      const lpPower = countPower + parseFloat(data.lpinfos[0].power ?? '0')
-      const tradPower = parseFloat(data.userMiningInfos[0].power ?? '0')
-      return lpPower + tradPower
+      const lpPower = countLpPower + parseFloat(data.lpinfos[0].power ?? '0')
+      return lpPower
     }
     return 0
   }, [data, error, loading])
+
+  const myTradPower = useMemo(() => {
+    if (!loading && !error && data && data.userMiningInfos.length) {
+      const tradPower = parseFloat(data.userMiningInfos[0].power ?? '0')
+      return tradPower
+    }
+    return 0
+  }, [data, error, loading])
+
+  const myTotalPower = useMemo(() => {
+    return myLpPower + myTradPower
+  }, [myLpPower, myTradPower])
 
   return (
     <StairCard bg={StairBgImage}>
       <RowBetween>
         <IQNumberWrapper>
           <IQNumberTitle>My Total Power</IQNumberTitle>
-          <IQNumber>{formatToFixed(myTotalPower.toString())}</IQNumber>
+          <IQNumber>{formatToFixed(myTotalPower) ?? '-'}</IQNumber>
         </IQNumberWrapper>
         <IQNumberWrapper>
           <IQNumberTitle>Trading Power</IQNumberTitle>
-          <IQNumber>
-            {(data && data?.userMiningInfos?.[0]?.power && formatToFixed(data?.userMiningInfos?.[0]?.power)) ?? '-'}
-          </IQNumber>
+          <IQNumber>{formatToFixed(myTradPower) ?? '-'}</IQNumber>
         </IQNumberWrapper>
         <IQNumberWrapper>
           <IQNumberTitle>LP Power</IQNumberTitle>
-          <IQNumber>{(data && data?.lpinfos?.[0]?.power && formatToFixed(data?.lpinfos?.[0]?.power)) ?? '-'}</IQNumber>
+          <IQNumber>{formatToFixed(myLpPower) ?? '-'}</IQNumber>
         </IQNumberWrapper>
         <IQNumberWrapper>
           <IQNumberTitle>Referral Power</IQNumberTitle>
